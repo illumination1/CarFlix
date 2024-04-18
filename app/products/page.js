dfe71fc39b30b05d9fd89759c514f9243a91926e
illumination1 <get2indra10@gmail.com>
2024-04-18 15:05:41 +0530

    add url

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,12 @@
-"use client";
-import ProductsList from "@/components/ProductsList";
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Home() {
-    return <ProductsList />;
+    return <ProductsList apiBaseUrl={apiBaseUrl} />;
 }
+
+// "use client";
+// import ProductsList from "@/components/ProductsList";
+
+// export default function Home() {
+//     return <ProductsList />;
+// }
