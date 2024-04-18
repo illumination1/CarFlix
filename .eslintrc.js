808a3541ea38481b45c163d02e77791d849c8a4e
illumination1 <get2indra10@gmail.com>
2024-04-18 15:57:45 +0530

    es lint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Other ESLint configuration...
+    rules: {
+      'react/jsx-no-undef': 'off',
+      // Other rules...
+    },
+  };
+  
--- app/products/page.js
+++ app/products/page.js
@@ -6,19 +6,14 @@
 //     return <ProductsList />;
 // }
 
-// import ProductsList from '@/components/ProductsList'; 
+import ProductsList from '@/components/ProductsList'; 
 
-// export default function ProductsPage() {
-//   return (
-//     <div>
-//       <h1>Products Page</h1>
-//       <ProductsList />
-//     </div>
-//   );
-// }
-
-import ProductsList from "@/components/ProductsList";
-
-export default function Home() {
-  return <ProductsList />;
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <ProductsList />
+    </div>
+  );
 }
+
