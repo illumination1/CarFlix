0e630a468256768cbab41b711688743b7e968934
illumination1 <get2indra10@gmail.com>
2024-04-18 16:15:09 +0530

    es lint

--- app/products/page.js
+++ app/products/page.js
@@ -5,14 +5,14 @@
 // export default function Home() {
 //     return <ProductsList />;
 // }
-
-import ProductsList from '@/components/ProductsList'; 
+"use client";
+import ProductList from '@/components/ProductsList'; 
 
 export default function ProductsPage() {
   return (
     <div>
       <h1>Products Page</h1>
-      <ProductsList />
+      <ProductList />
     </div>
   );
 }
