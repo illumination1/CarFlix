939cfe3589c7f571e50b4efc2dadc8562bac64dd
illumination1 <get2indra10@gmail.com>
2024-04-18 15:32:20 +0530

    product list

--- app/products/page.js
+++ app/products/page.js
@@ -1,18 +1,18 @@
 
-// "use client";
-// import ProductsList from "@/components/ProductsList";
+"use client";
+import ProductsList from "@/components/ProductsList";
 
-// export default function Home() {
-//     return <ProductsList />;
-// }
-
-import ProductsList from '@/components/ProductsList'; // Make sure the path is correct based on your project structure
-
-export default function ProductsPage() {
-  return (
-    <div>
-      <h1>Products Page</h1>
-      <ProductsList />
-    </div>
-  );
+export default function Home() {
+    return <ProductsList />;
 }
+
+// import ProductsList from '@/components/ProductsList'; 
+
+// export default function ProductsPage() {
+//   return (
+//     <div>
+//       <h1>Products Page</h1>
+//       <ProductsList />
+//     </div>
+//   );
+// }
